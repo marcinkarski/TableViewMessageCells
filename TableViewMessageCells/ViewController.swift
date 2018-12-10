89c5b9d5e8c9147b2c775ec4fdd1f24c3f16b419
Marcin Karski <karski80@gmail.com>
2018-12-10 18:12:29 +0000

    Initial Commit

---
+++ TableViewMessageCells/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewMessageCells
+//
+//  Created by Marcin Karski on 10/12/2018.
+//  Copyright © 2018 Marcin Karski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
