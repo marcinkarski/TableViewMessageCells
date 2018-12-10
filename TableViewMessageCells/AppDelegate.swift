0d2c4379b98ecbe63ae2c8d9be7674f588fbed58
Marcin Karski <karski80@gmail.com>
2018-12-10 19:05:31 +0000

    Basic Table Cell setup

--- TableViewMessageCells/AppDelegate.swift
+++ TableViewMessageCells/AppDelegate.swift
@@ -7,9 +7,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        let viewController = ViewController()
+        let viewController = UINavigationController(rootViewController: ViewController())
         
-        window = UIWindow(frame: UIScreen.main.bounds)
+        window = UIWindow()
         window?.rootViewController = viewController
         window?.makeKeyAndVisible()
         
--- TableViewMessageCells/ViewController.swift
+++ TableViewMessageCells/ViewController.swift
@@ -1,9 +1,24 @@
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    private let identifier = "Cell"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
+        view.backgroundColor = .white
+        self.title = "Messages"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: identifier)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        cell.textLabel?.text = "Text"
+        return cell
     }
 }
