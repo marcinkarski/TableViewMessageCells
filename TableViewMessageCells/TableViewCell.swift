b5dda57b29f558a8bdc7524de7b081e9c6f8454c
Marcin Karski <karski80@gmail.com>
2018-12-10 20:00:23 +0000

    Text message implementation

---
+++ TableViewMessageCells/TableViewCell.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    let label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.text = "With our screen full designed, you'll see a nagging bug that occurs when you pull up the keyboard. We'll have to account for the keyboard's size whenever its being presented by shifting our view up through a translation transformation."
+        return label
+    }()
+    
+    let background: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 8
+        view.backgroundColor = .yellow
+        return view
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(background)
+        addSubview(label)
+        NSLayoutConstraint.activate([label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 32), label.topAnchor.constraint(equalTo: topAnchor, constant: 32), label.widthAnchor.constraint(equalToConstant: 240), label.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -16), background.leadingAnchor.constraint(equalTo: label.leadingAnchor, constant: -16), background.topAnchor.constraint(equalTo: label.topAnchor, constant: -16), background.trailingAnchor.constraint(equalTo: label.trailingAnchor, constant: 16), background.bottomAnchor.constraint(equalTo: label.bottomAnchor, constant: 16)])
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
--- TableViewMessageCells/ViewController.swift
+++ TableViewMessageCells/ViewController.swift
@@ -3,22 +3,25 @@ import UIKit
 class ViewController: UITableViewController {
     
     private let identifier = "Cell"
+    
+    let messages = ["This is my first message", "With our screen full designed, you'll see a nagging bug that occurs when you pull up the keyboard. We'll have to account for the keyboard's size whenever its being presented by shifting our view up through a translation transformation.", "We'll have to account for the keyboard's size whenever its being presented by shifting our view up through a translation transformation."]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         self.title = "Messages"
         navigationController?.navigationBar.prefersLargeTitles = true
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: identifier)
+        tableView.register(TableViewCell.self, forCellReuseIdentifier: identifier)
+        tableView.separatorStyle = .none
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return messages.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
-        cell.textLabel?.text = "Text"
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath) as! TableViewCell
+        cell.label.text = messages[indexPath.row]
         return cell
     }
 }
